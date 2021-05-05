630859457ac2e02884e085b998f64076edeea739
Ron TeKippe <tekippr3@nationwide.com>
2021-05-05 18:28:48 -0400

    final commit

---
+++ src/main/java/edu/cscc/degrees/api/DegreesPostController.java
@@ -0,0 +1,22 @@
+package edu.cscc.degrees.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/menu/catergories")
+public class DegreesPostController {
+
+    @PostMapping
+    public ResponseEntity createBlogPost() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost/api/menu/categories/1");
+        return new ResponseEntity("", headers,HttpStatus.CREATED);
+    }
+
+}
